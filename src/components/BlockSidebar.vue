2543aedca36fa9b187301a33c0579ef45e40a16e
zurakusrashvili <zkusrashvili@gmail.com>
2024-07-30 15:15:53 +0400

    fixed: blocksidebar

--- src/components/BlockSidebar.vue
+++ src/components/BlockSidebar.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="sidebar">
     <ul>
-      <li v-for="(block, index) in blocks" :key="block.position" :style="{ top: `${block.top}px`, left: '5px', position: 'absolute' }" @click="selectBlock(block)">
+      <li v-for="(block, index) in blocks" :key="block.position"
+        :style="{ top: `${block.top}px`, left: '5px', position: 'absolute' }" @click="selectBlock(block)">
         <i v-html="block.icon"></i>
-        <span class="block-name">{{block.type}}</span>
+        <span class="block-name">{{ block.type }}</span>
       </li>
     </ul>
     <div class="add-block" @click="toggleAddMenu">
       <i class="fa-regular fa-plus"></i>
-      <AddBlockMenu ref="addBlockMenu" :visible="addMenuVisible" :basic-blocks="basicBlocks" :advanced-blocks="advancedBlocks" @add-block="addBlock" />
+      <AddBlockMenu ref="addBlockMenu" :visible="addMenuVisible" :basic-blocks="basicBlocks"
+        :advanced-blocks="advancedBlocks" @add-block="addBlock" />
     </div>
   </div>
 </template>
@@ -41,21 +43,31 @@ export default {
   },
   data() {
     return {
-      addMenuVisible: false,
       basicBlocks: [
-        // Your basic blocks here
+        { name: 'text', icon: 'fa-regular fa-text', displayName: 'Text', description: 'Just good old plain text' },
+        { name: 'headline', icon: 'fa-regular fa-heading', displayName: 'Headline', description: 'Headlines and subsections' },
+        { name: 'table', icon: 'fa-regular fa-table', displayName: 'Table', description: 'Simple tables' },
+        { name: 'list', icon: 'fa-regular fa-list', displayName: 'List', description: 'Simple lists (enumerated, itemized, etc.)' },
+        { name: 'image', icon: 'fa-regular fa-image', displayName: 'Image', description: 'Simple image from URL or media library' },
+        { name: 'code', icon: 'fa-regular fa-code', displayName: 'Code', description: 'Add a code section' },
       ],
       advancedBlocks: [
-        // Your advanced blocks here
+        { name: 'alert', icon: 'fa-regular fa-exclamation-triangle', displayName: 'Alert', description: 'An alert block' },
+        { name: 'banner', icon: 'fa-regular fa-bars', displayName: 'Banner', description: 'A banner with text and image' },
+        { name: 'cards', icon: 'fa-regular fa-id-card', displayName: 'Cards', description: 'A series of vertically arranged cards' },
+        { name: 'magic-block', icon: 'fa-regular fa-magic', displayName: 'Magic Block', description: 'Bring your own blocks with handlebars' },
       ],
+      addMenuVisible: false,
     }
   },
   mounted() {
     // this.updateBlocks()
     // this.editor.on('transaction', this.updateBlocks)
+    document.addEventListener('click', this.handleClickOutside);
   },
   beforeUnmount() {
     // this.editor.off('transaction', this.updateBlocks)
+    document.removeEventListener('click', this.handleClickOutside);
   },
   methods: {
     updateBlocks() {
@@ -86,7 +98,14 @@ export default {
       // You need to define how you want to map block types to names
     },
 
-    
+    handleClickOutside(event) {
+      const addBlockMenu = this.$refs.addBlockMenu ? this.$refs.addBlockMenu.$el : null;
+      if (this.addMenuVisible && !this.$el.contains(event.target) && !(addBlockMenu && addBlockMenu.contains(event.target))) {
+        this.addMenuVisible = false;
+      }
+    },
+
+
   },
 }
 </script>
@@ -94,7 +113,7 @@ export default {
 <style scoped>
 .sidebar {
   width: 50px;
-  height:2400px;
+  height: 2400px;
   background-color: #f4f4f4;
   padding: 2;
   border-right: 1px solid #ddd;
@@ -103,7 +122,7 @@ export default {
   flex-direction: column;
   justify-content: space-between;
   z-index: 999;
-  top:0;
+  top: 0;
 }
 
 .sidebar ul {
